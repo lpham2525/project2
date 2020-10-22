315f144c76859531f34f1a84e0dcdacc98783faf
LanChi <lpham2525@gmail.com>
2020-10-22 16:39:18 -0400

    tried out userRoutes

--- models/index.js
+++ models/index.js
@@ -1,9 +1,11 @@
 const Artist = require('./Artist.js')
 const Event = require('./Event.js')
 const Item = require('./Item.js')
+const User = require('./User.js')
 
 Artist.hasMany(Item)
 Event.belongsTo(Artist)
 Item.belongsTo(Artist)
+User.hasMany(Item)
 
-module.exports = { Artist, Event, Item }
+module.exports = { Artist, Event, Item, User }
