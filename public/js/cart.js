1c3fc83683ffd32076c178cd4b27c35c18c88469
LanChi <lpham2525@gmail.com>
2020-11-28 14:11:34 -0500

    revised const deleteItem in cart.js

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,3 +1,31 @@
+const { default: Axios } = require("axios")
+
 document.getElementById('checkOut').addEventListener('click', () => {
   window.location.replace('/checkout')
+})
+
+const getItems = () => {
+  axios.get('/api/items')
+    .then(({ data }) => {
+      document.getElementById('listItems').innerHTML = ''
+      data.forEach(item => {
+        let itemElem = document.createElement('li')
+        itemElem.innerHTML = `
+        <button class = 'deleteItem' data-item="${item.text}">Delete</button> `
+        document.getElementById('listItems').append(itemElem)
+      })
+    })
+    .catch(err => console.log(err))
+}
+
+const deleteItem = id => {
+  axios.delete(`/api/items/${id}`)
+    .then(() => {
+      document.getElementById(`${id}`).remove()
+    })
+    .catch(err => console.error(err))
+}
+
+document.addEventListener('click', event => {
+  deleteItem(event.target.dataset.id)
 })
