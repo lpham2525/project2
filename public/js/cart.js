7a82b85a2870c5558d117a5833b0bbb1ef98df0c
LanChi <lpham2525@gmail.com>
2020-12-14 10:49:50 -0500

    changed span for deleting card to a button for deleting the card

--- public/js/cart.js
+++ public/js/cart.js
@@ -2,33 +2,32 @@ document.getElementById('checkOut').addEventListener('click', () => {
   window.location.replace('/checkout')
 })
 
-const getItems = () => {
-  axios.get('/api/items')
-    .then(({ data }) => {
-      document.getElementById('listItems').innerHTML = ''
-      data.forEach(item => {
-        let itemElem = document.createElement('div')
-        itemElem.className = 'card'
-        itemElem.innerHTML = `
-        <a href="#">
-      <div class="card-image">
-        <img src="{{img}}">
-      </div>
-      <div class="card-action">
-        {{title}}
-      </div>
-      </a>
-      <a href="#">
-      <div class="card-action">
-        <span style='color:red' data-id={{id}}>Delete</span>
-      </div>
-      </a>
-    </div>`
-        document.getElementById('listItems').append(itemElem)
-      })
-    })
-    .catch(err => console.log(err))
-}
+// const getItems = () => {
+//   axios.get('/api/items')
+//     .then(({ data }) => {
+//       document.getElementById('listItems').innerHTML = ''
+//       data.forEach(item => {
+//         let itemElem = document.createElement('div')
+//         itemElem.className = 'card'
+//         itemElem.innerHTML = `
+//         <a href="#">
+//       <div class="card-image">
+//         <img src="{{img}}">
+//       </div>
+//       <div class="card-action">
+//         {{title}}
+//       </div>
+//       </a>
+//       <a href="#">
+//       <div class="card-action">
+//         <span style='color:red' data-id={{id}}>Delete</span>
+//       </div>
+//       </a>`
+//         document.getElementById('listItems').append(itemElem)
+//       })
+//     })
+//     .catch(err => console.log(err))
+// }
 
 const deleteItem = id => {
   axios.delete(`/api/items/${id}`)
@@ -42,6 +41,7 @@ document.addEventListener('click', event => {
   if (event.target.className === 'deleteItem') {
     axios.delete(`/api/items/${event.target.dataset.id}`)
       .then(() => {
+        console.log(event)
         event.target.parentNode.remove()
       })
       .catch(err => console.error(err))
