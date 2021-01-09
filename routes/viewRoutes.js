ed285cb7a0037b31e745440613fc44377f978c47
LanChi <lpham2525@gmail.com>
2021-01-08 20:08:41 -0500

    changed Recently Featured cards on homepage

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -61,20 +61,21 @@ router.get('/dashboard', (req, res) => {
 
 router.get('/product', (req, res) => {
   const featured = [{
-    // src: '/assets/Annie_Leibovitz/Images/star-wars-the-rise-of-skywalker-vanity-fair 2.jpg',
     src: 'https://i.imgur.com/vqJyZHA.jpg',
     title: 'Star Wars: The Rise of Skywalker',
     description: `HORSING AROUND:
-Finn and new ally Jannah (Naomi Ackie), atop hardy orbaks, lead the charge against the mechanized forces of the First Order. “It’s extremely surreal to be in it,” says Ackie, “and see how it works from the inside.”`, id: 1 }]
+Finn and new ally Jannah (Naomi Ackie), atop hardy orbaks, lead the charge against the mechanized forces of the First Order. “It’s extremely surreal to be in it,” says Ackie, “and see how it works from the inside.”`,
+    id: 1
+  }]
 
   const artistBio = [
     { src: '/assets/header-images/annie-leibovitz-portrait.jpg', bio: "Annie Leibovitz is an American portrait photographer. She is best known for her engaging portraits, particularly of celebrities, which often feature subjects in intimate settings and poses. The Library of Congress declared her a Living Legend, and she is the first woman to have a feature exhibition at Washington's National Portrait Gallery." }
   ]
 
   const artImgs = [
-    { src: '/assets/Annie_Leibovitz/Images/Angelina-Jolie-Portrait 2.jpg', title: 'Angelina Jolie Portrait', id: 2, description: `Angelina Jolie against the backdrop of Notre Dame. Photograph for Disney Dream Portraits` },
-    { src: '/assets/Annie_Leibovitz/Images/willie-nelson.jpg', title: 'Willie Nelson', id: 3, description: `Photographed at Luck Ranch in Spicewood, Texas; photograph on silver gelatin print` },
-    { src: '/assets/Annie_Leibovitz/Images/Queen_Elizabeth_II.jpg', title: 'Queen Elizabeth II at Buckingham Palace, London, 2007', id: 4 , description: `The first photograph of the Queen taken by an American photographer.` }]
+    { src: '/assets/Annie_Leibovitz/Images/Angelina-Jolie-Portrait 2.jpg', title: 'Angelina Jolie Portrait', id: 2, description: 'Angelina Jolie against the backdrop of Notre Dame. Photograph for Disney Dream Portraits' },
+    { src: '/assets/Annie_Leibovitz/Images/willie-nelson.jpg', title: 'Willie Nelson', id: 3, description: 'Photographed at Luck Ranch in Spicewood, Texas; photograph on silver gelatin print.' },
+    { src: '/assets/Annie_Leibovitz/Images/Queen_Elizabeth_II.jpg', title: 'Queen Elizabeth II at Buckingham Palace, London, 2007', id: 4 , description: 'The first photograph of the Queen taken by an American photographer.' }]
 
   res.render('product', {
     page: {
